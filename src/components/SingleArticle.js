039021bbcbf885d76200d9c87c9c55caf1bfeae3
JohnSwan <johneswan3@protonmail.com>
2021-10-22 18:36:28 -0600

    Finished Article Page. Started Single Article

--- src/components/SingleArticle.js
+++ src/components/SingleArticle.js
@@ -1,3 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import sanityClient from '../client.js';
+import { useParams } from 'react-router-dom';
+import BlockContent from '@sanity/block-content-to-react';
+
 export default function SingleArticle() {
-  return <h1>Single Article</h1>;
+  const [singleArticle, setSingleArticle] = useState(null);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[slug.current =="${slug}"] {
+        title,
+        _id,
+        slug,
+        "name": author->name,
+        "authorImage": author->image,
+        publishDate,
+        tags,
+        heading,
+        article,
+        mainImage{
+          asset->{
+            _id,
+            url
+          },
+          alt,
+        },
+        secondaryImage{
+          asset->{
+            _id,
+            url
+          },
+          alt,
+        },
+        carousel{
+          asset->{
+            _id,
+            url
+          },
+          alt,
+        },
+      }`
+      )
+      .then((data) => setSingleArticle(data[0]))
+      .catch(console.error);
+  }, [slug]);
+
+  if (!singleArticle)
+    return (
+      <div className="min-h-screen bg-gray-800 text-gray-100 text-4xl">
+        Loading...
+      </div>
+    );
+
+  return (
+    <main className="bg-gray-800 min-h-screen">
+      <section></section>
+    </main>
+  );
 }
