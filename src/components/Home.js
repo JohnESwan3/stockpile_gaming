6fa9590dc9c749d00d6d704cee05e9e89474a100
JohnSwan <johneswan3@protonmail.com>
2021-11-08 19:22:41 -0700

    Navbar Tweaks

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,11 @@ export default function Home() {
         className="fixed object-cover w-full h-full before:absolute before:inset-0 filter brightness-50 z-0"
       />
       <section className="relative flex-column justify-center text-center pt-12 px-8 ">
-        <img src={logo} alt="" className="w-1/2 md:w-1/3 m-auto" />
+        <img
+          src={logo}
+          alt="Stockpile Gaming Logo"
+          className="w-1/2 md:w-1/3 m-auto"
+        />
         <h1 className="text-green-400 text-3xl font-bold leading-none lg:leading-snug">
           Stockpile Gaming
         </h1>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,52 +13,54 @@ import { MdArticle } from 'react-icons/md';
 
 export default function NavBar() {
   return (
-    <nav className="">
-      <div className="">
-        <div className="menu-bar flex flex-col top-0 right-0 fixed z-50  w-16 m-0 bg-gray-800 border-r-2 border-gray-700 shadow-xl text-gray-100">
-          <div className="flex flex-col top-0 right-0 z-50">
-            <button className="outline-none mobile-menu-button">
-              <svg
-                className="w-16 h-16 text-gray-200 hover:text-green-500"
-                x-show="!showMenu"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
-            </button>
-            <div className="hidden mobile-menu ">
-              <ul>
-                <li>
-                  <NavLink to="/" exact className="">
-                    <img
-                      src={SPLogo}
-                      alt=""
-                      className="p-1 sidebar-icon-logo"
-                    />
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/review">
-                    <SideBarIcon icon={<GiConsoleController size="32" />} />
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/article">
-                    <SideBarIcon icon={<MdArticle size="32" />} />
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/about">
-                    <SideBarIcon icon={<GiCowled size="32" />} />
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
+    <nav>
+      <div className="menu-bar flex flex-col top-0 right-0 fixed z-50  w-16 m-0 bg-gray-800 border-l-2 border-b-2 border-gray-700 shadow text-gray-100">
+        <div className="flex flex-col top-0 right-0 z-50">
+          <button className="outline-none mobile-menu-button">
+            <svg
+              className="w-16 h-16 text-gray-200 "
+              x-show="!showMenu"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </button>
+          <div className="hidden mobile-menu text-center">
+            <ul>
+              <li>
+                <NavLink to="/" exact className="">
+                  <img
+                    src={SPLogo}
+                    alt="Home"
+                    className="p-1 sidebar-icon-logo"
+                  />
+                  <p className="text-xs">Home</p>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/review">
+                  <SideBarIcon icon={<GiConsoleController size="32" />} />
+                </NavLink>
+                <p className="text-xs">Reviews</p>
+              </li>
+              <li>
+                <NavLink to="/article">
+                  <SideBarIcon icon={<MdArticle size="32" />} />
+                  <p className="text-xs">Articles</p>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">
+                  <SideBarIcon icon={<GiCowled size="32" />} />
+                  <p className="text-xs">About</p>
+                </NavLink>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
